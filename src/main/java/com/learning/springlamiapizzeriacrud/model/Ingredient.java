d34b1ec53117f615a57e3142ffa8f05e44b5cc97
GianmarcoOrabona <gianmarcoorabona091@gmail.com>
2024-01-18 14:29:14 +0100

    add ingredient entity and repository

---
+++ src/main/java/com/learning/springlamiapizzeriacrud/model/Ingredient.java
@@ -0,0 +1,33 @@
+package com.learning.springlamiapizzeriacrud.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "ingredients")
+public class Ingredient {
+    // ATTRIBUTI
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String name;
+
+    // GETTER E SETTER
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
